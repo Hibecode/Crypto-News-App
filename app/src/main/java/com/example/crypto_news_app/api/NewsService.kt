71a766591a36d0f55ea191072163bd85d0ce8d56
Hibecode <ibkokunoye@gmail.com>
2022-05-20 23:35:41 +0100

    Add first api endpoint with queries

--- app/src/main/java/com/example/crypto_news_app/api/NewsService.kt
+++ app/src/main/java/com/example/crypto_news_app/api/NewsService.kt
@@ -3,11 +3,21 @@ package com.example.crypto_news_app.api
 import com.example.crypto_news_app.RetrofitResp.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsService {
 
-    @GET("v2/everything?q=crypto")
+    @GET("v2/top-headlines")
     fun getBreakingNews(
-
+        @Query("country")
+        countryCode: String,
+        @Query("q")
+        search: String = "crypto",
+        @Query("page")
+        pageNumber: String,
+        @Query("apiKey")
+        apiKey: String = "rn"
     ) : Response<NewsResponse>
+
+
 }
