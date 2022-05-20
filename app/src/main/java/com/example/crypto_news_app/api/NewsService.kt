4072438614a725e03da01eac79594a47d5d4dbe3
Hibecode <ibkokunoye@gmail.com>
2022-05-21 00:52:30 +0100

    Add Retrofit Instance

--- app/src/main/java/com/example/crypto_news_app/api/NewsService.kt
+++ app/src/main/java/com/example/crypto_news_app/api/NewsService.kt
@@ -2,7 +2,12 @@ package com.example.crypto_news_app.api
 
 import com.example.crypto_news_app.RetrofitResp.NewsResponse
 import com.example.crypto_news_app.Utils.Constants.Companion.API_KEY
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -29,6 +34,31 @@ interface NewsService {
         @Query("apiKey")
         apiKey: String = API_KEY
     ) : Response<NewsResponse>
+
+
+    companion object {
+        private const val BASE_URL = "https://newsapi.org"
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+
+        val create: NewsService by lazy {
+            retrofit.create(NewsService::class.java)
+        }
+    }
 
 
 }
