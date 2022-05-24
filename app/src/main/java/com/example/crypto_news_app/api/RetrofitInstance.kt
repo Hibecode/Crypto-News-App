d8d9f1d9001b7e237b4e41b8551d830a52714a27
Hibecode <ibkokunoye@gmail.com>
2022-05-24 13:42:19 +0100

    Add retrofit instance code to allow direct calling of api

--- app/src/main/java/com/example/crypto_news_app/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/crypto_news_app/api/RetrofitInstance.kt
@@ -7,6 +7,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
 
+    companion object {
+        private const val BASE_URL = "https://newsapi.org"
 
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+
+        val api: NewsService by lazy {
+            retrofit.create(NewsService::class.java)
+        }
+    }
 }
