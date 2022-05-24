85819410300f04c1eb163242205830529653798a
Hibecode <ibkokunoye@gmail.com>
2022-05-24 12:15:11 +0100

    Add DB impl in full

--- app/src/main/java/com/example/crypto_news_app/db/ArticleDB.kt
+++ app/src/main/java/com/example/crypto_news_app/db/ArticleDB.kt
@@ -1,9 +1,44 @@
 package com.example.crypto_news_app.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.crypto_news_app.models.Article
 
-
 @Database(entities = [Article::class], version = 1)
-abstract class ArticleDB {
+abstract class ArticleDB: RoomDatabase() {
+
+    abstract fun articleDao(): ArticleDao
+
+
+    companion object{
+        //using @Volatile make writes to this field visible to other threads
+        @Volatile
+        private var instance: ArticleDB? = null
+        private val LOCK = Any()
+
+
+        //invoke() operator is used to allows instances of the class to be called
+        //without a method name
+        operator fun invoke(context: Context) =
+            //checks if instance is null, if it isn't,
+            // it uses a synchronized block; where it checks if instance is still null, then,
+            //it creates the database and assigns the created db to the instance variable
+            instance ?: synchronized(LOCK) {
+                //using synchronized prevents other threads from accessing this block
+                //LOCK serves as a lock to the block
+                instance ?: createDatabase(context).also { instance = it }
+            }
+
+
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDB::class.java, "article_db"
+            ).build()
+
+
+    }
 }
