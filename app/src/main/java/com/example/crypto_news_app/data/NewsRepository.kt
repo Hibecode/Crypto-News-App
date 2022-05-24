6eab1912cd2960ba6ed380113afa58658810f6a6
Hibecode <ibkokunoye@gmail.com>
2022-05-24 13:32:06 +0100

    Update repo with dao functions

--- app/src/main/java/com/example/crypto_news_app/data/NewsRepository.kt
+++ app/src/main/java/com/example/crypto_news_app/data/NewsRepository.kt
@@ -1,4 +1,17 @@
 package com.example.crypto_news_app.data
 
-class NewsRepository {
+import com.example.crypto_news_app.api.NewsService
+import com.example.crypto_news_app.db.ArticleDB
+import com.example.crypto_news_app.models.Article
+
+class NewsRepository(private val db: ArticleDB) {
+
+
+    fun getAllArticles() = db.articleDao().getAllArticles()
+
+    suspend fun upsertArticle(article: Article) = db.articleDao().upsert(article)
+
+    suspend fun deleteArticle(article: Article) = db.articleDao().delete(article)
+
+
 }
