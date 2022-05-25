4833dcef42c7f1dc09f5f775e623e304dbc1ae03
Hibecode <ibkokunoye@gmail.com>
2022-05-25 14:19:20 +0100

    Fix:Make the NewViewModel extend ViewModel

--- app/src/main/java/com/example/crypto_news_app/ui/NewsVIewModel.kt
+++ app/src/main/java/com/example/crypto_news_app/ui/NewsVIewModel.kt
@@ -1,8 +1,20 @@
 package com.example.crypto_news_app.ui
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.crypto_news_app.data.NewsRepository
+import com.example.crypto_news_app.models.Article
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(repository: NewsRepository) {
+class NewsViewModel(repository: NewsRepository): ViewModel() {
 
+    val newResponse: MutableLiveData<Response<Article>> = MutableLiveData()
+
+
+    fun getBreakingNews() = viewModelScope.launch {
+
+    }
 
 }
