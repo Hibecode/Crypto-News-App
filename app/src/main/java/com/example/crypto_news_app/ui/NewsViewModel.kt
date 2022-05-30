6ab8e9728600212f3c3855047e2a76a39b6a9ae6
Hibecode <ibkokunoye@gmail.com>
2022-05-30 18:43:43 +0100

    Update change name

--- app/src/main/java/com/example/crypto_news_app/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/crypto_news_app/ui/NewsViewModel.kt
@@ -4,24 +4,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.crypto_news_app.data.NewsRepository
-import com.example.crypto_news_app.models.Article
 import com.example.crypto_news_app.models.NewsResponse
 import com.example.crypto_news_app.utils.Resource
 import kotlinx.coroutines.launch
 import retrofit2.Response
-import java.lang.Exception
 
 class NewsViewModel(val repository: NewsRepository): ViewModel() {
 
-    val breakingNewsResponse: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var homeNewsPageNo = 1
     var searchCategory = "crypto"
 
 
     fun getBreakingNews() = viewModelScope.launch {
-        breakingNewsResponse.postValue(Resource.Loading())
+        breakingNews.postValue(Resource.Loading())
         val response = repository.getBreakingNews("us", searchCategory, homeNewsPageNo)
-        breakingNewsResponse.postValue(handlingBreakingNews(response))
+        breakingNews.postValue(handlingBreakingNews(response))
     }
 
     private fun handlingBreakingNews(response: Response<NewsResponse>): Resource<NewsResponse> {
