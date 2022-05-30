5ae8f2ab340b7be5d335356dcadfa96fff05a632
Hibecode <ibkokunoye@gmail.com>
2022-05-30 18:58:42 +0100

    Update with viewmodel

--- app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
+++ app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
@@ -2,12 +2,16 @@ package com.example.crypto_news_app.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.crypto_news_app.NewsActivity
 import com.example.crypto_news_app.R
+import com.example.crypto_news_app.models.NewsResponse
 import com.example.crypto_news_app.ui.NewsAdapter
 import com.example.crypto_news_app.ui.NewsViewModel
+import com.example.crypto_news_app.utils.Resource
 import kotlinx.android.synthetic.main.fragment_home_news.*
 
 class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
@@ -20,6 +24,21 @@ class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
         viewModel = (activity as NewsActivity).viewModel
 
         setUpRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    response.data?.let { newsResponse ->
+                        myAdapter.differ.submitList(newsResponse.articles) }
+                }
+                is Resource.Error -> {
+                    Toast.makeText(activity, response.message, Toast.LENGTH_LONG).show()
+                }
+                is Resource.Loading -> {
+                    Toast.makeText(activity, "LOADING", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
 
     }
 
