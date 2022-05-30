87c81e0e9b4a236e8c967d56d18662c8179bb450
Hibecode <ibkokunoye@gmail.com>
2022-05-30 18:03:43 +0100

    Update Home fragment with onViewCreated

--- app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
+++ app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
@@ -1,7 +1,14 @@
 package com.example.crypto_news_app.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.crypto_news_app.R
 
 class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
