8a61795aace903549e5de71613c65cd7baaaa5e0
Hibecode <ibkokunoye@gmail.com>
2022-05-30 18:27:42 +0100

    Update with recyclerView

--- app/src/main/java/com/example/crypto_news_app/NewsActivity.kt
+++ app/src/main/java/com/example/crypto_news_app/NewsActivity.kt
@@ -2,10 +2,12 @@ package com.example.crypto_news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.crypto_news_app.data.NewsRepository
 import com.example.crypto_news_app.db.ArticleDB
 import com.example.crypto_news_app.models.Article
@@ -13,6 +15,7 @@ import com.example.crypto_news_app.ui.NewsAdapter
 import com.example.crypto_news_app.ui.NewsViewModel
 import com.example.crypto_news_app.ui.NewsViewModelFactory
 import kotlinx.android.synthetic.main.activity_news.*
+import kotlinx.android.synthetic.main.fragment_home_news.*
 
 class NewsActivity : AppCompatActivity() {
 
@@ -24,6 +27,8 @@ class NewsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_news)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 
+        setUpRecyclerView()
+
         val db = ArticleDB(this)
         val repository = NewsRepository(db)
         val viewModelFactory = NewsViewModelFactory(repository)
@@ -33,6 +38,9 @@ class NewsActivity : AppCompatActivity() {
 
     }
 
-
+    fun setUpRecyclerView() {
+        rvHomeNews.adapter = myAdapter
+        rvHomeNews.layoutManager = LinearLayoutManager(this)
+    }
 
 }
