5c56c96a6b1e75681c3a0dcc231204dc7eef7829
Hibecode <ibkokunoye@gmail.com>
2022-05-30 19:08:59 +0100

    Update class with methods

--- app/src/main/java/com/example/crypto_news_app/db/Converters.kt
+++ app/src/main/java/com/example/crypto_news_app/db/Converters.kt
@@ -1,4 +1,18 @@
 package com.example.crypto_news_app.db
 
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.example.crypto_news_app.models.Source
+
 class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String{
+        return source.name
+    }
+
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+
 }
