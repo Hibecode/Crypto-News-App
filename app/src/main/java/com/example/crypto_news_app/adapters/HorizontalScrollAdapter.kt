ca420fdc94df5e570a64b904af70557f85d7644e
Hibecode <ibkokunoye@gmail.com>
2022-06-08 00:49:25 +0100

    Add horizontal scroll adapter

--- app/src/main/java/com/example/crypto_news_app/adapters/HorizontalScrollAdapter.kt
+++ app/src/main/java/com/example/crypto_news_app/adapters/HorizontalScrollAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.crypto_news_app.adapters
 
-class HorizontalScrollAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class HorizontalScrollAdapter: RecyclerView.Adapter<HorizontalScrollAdapter.HorizontalScrollViewHolder>() {
+
+
+    inner class HorizontalScrollViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HorizontalScrollViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HorizontalScrollViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
