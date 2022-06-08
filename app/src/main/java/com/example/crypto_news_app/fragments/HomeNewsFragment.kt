5adc45528024c61d30bb15d69f29e46fe9aa6af7
Hibecode <ibkokunoye@gmail.com>
2022-06-08 02:16:08 +0100

    Add tag adapter to home fragment

--- app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
+++ app/src/main/java/com/example/crypto_news_app/fragments/HomeNewsFragment.kt
@@ -8,7 +8,9 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.crypto_news_app.NewsActivity
 import com.example.crypto_news_app.R
+import com.example.crypto_news_app.adapters.HorizontalScrollAdapter
 import com.example.crypto_news_app.adapters.NewsAdapter
+import com.example.crypto_news_app.models.Tag
 import com.example.crypto_news_app.ui.NewsViewModel
 import com.example.crypto_news_app.utils.Resource
 import kotlinx.android.synthetic.main.fragment_home_news.*
@@ -16,6 +18,7 @@ import kotlinx.android.synthetic.main.fragment_home_news.*
 class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
 
     val myAdapter by lazy { NewsAdapter() }
+    val TagAdapter by lazy { HorizontalScrollAdapter() }
     lateinit var viewModel: NewsViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,6 +26,7 @@ class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
         viewModel = (activity as NewsActivity).viewModel
 
         setUpRecyclerView()
+        setUpTagRecyclerView()
 
         myAdapter.setOnItemClickListener {
             //Puts the article data into bundle
@@ -59,5 +63,27 @@ class HomeNewsFragment: Fragment(R.layout.fragment_home_news) {
         rvHomeNews.adapter = myAdapter
         rvHomeNews.layoutManager = LinearLayoutManager(activity)
     }
+
+    private fun setUpTagRecyclerView() {
+        rvHorizontalScroll.adapter = TagAdapter
+        rvHorizontalScroll.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        TagAdapter.differ.submitList(TagList())
+    }
+
+    private fun TagList() =
+        listOf<Tag>(
+            Tag("Blockchain"),
+            Tag("Bitcoin"),
+            Tag("Ethereum"),
+            Tag("Altcoin"),
+            Tag("Crypto"),
+            Tag("DAO"),
+            Tag("P2P"),
+            Tag("dApp"),
+            Tag("NFT"),
+            Tag("Solidity"),
+            Tag("Token")
+        )
+
 
 }
