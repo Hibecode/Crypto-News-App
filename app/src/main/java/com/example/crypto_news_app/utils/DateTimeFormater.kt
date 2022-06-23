f9f7456849b9bf06e9128748fa8dd6f367e1e9ad
Hibecode <ibkokunoye@gmail.com>
2022-06-23 16:02:48 +0100

    Add period working function

--- app/src/main/java/com/example/crypto_news_app/utils/DateTimeFormater.kt
+++ app/src/main/java/com/example/crypto_news_app/utils/DateTimeFormater.kt
@@ -3,18 +3,16 @@ package com.example.crypto_news_app.utils
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import java.text.SimpleDateFormat
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import java.util.*
 
 
 @RequiresApi(Build.VERSION_CODES.O)
-fun parseDateTime(/*dateTime: String*/) {
+fun parseDateTime(dateTime: String) {
 
 
     val current = LocalDateTime.now()
@@ -27,12 +25,12 @@ fun parseDateTime(/*dateTime: String*/) {
 
 
 
-    val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.UK)
+    /*val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.UK)
     //formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
     formatter.timeZone.id = "UTC"
     val formatted = formatter.parse("2022-06-16T19:31:39Z")
     val formatted2 = formatter.parse("2022-06-16T22:31:39Z")
-    Log.d("parseTest", formatted.toLocaleString())
+    Log.d("parseTest", formatted.toLocaleString())*/
 
     val dtf = DateTimeFormatter.ISO_DATE_TIME
     //val zdt = ZonedDateTime.parse("2022-06-16T22:31:39Z", dtf)
@@ -45,13 +43,67 @@ fun parseDateTime(/*dateTime: String*/) {
 
     //Log.d("parseTest", zdt.month.toString())
 
+    val noOfMinutes = ChronoUnit.MINUTES.between(ldt, zdt)
     val noOfHours = ChronoUnit.HOURS.between(ldt, zdt)
     val noOfDays = ChronoUnit.DAYS.between(ldt, zdt)
     val noOfMonths = ChronoUnit.MONTHS.between(ldt, zdt)
+    val noOfYears = ChronoUnit.YEARS.between(ldt, zdt)
 
     Log.d("parseTest", noOfDays.toString())
 
 
 
+
+
 }
 
+/**
+ * Returns instant zoned date and time
+ *
+ * @return ZonedDateTime
+ */
+@RequiresApi(Build.VERSION_CODES.O)
+fun getInstantTime(): ZonedDateTime {
+    val zoneId: ZoneId = ZoneId.of("Africa/Lagos") // Or "Asia/Kolkata", "Europe/Paris", and so on.
+    val instant: Instant = Instant.now() // Current moment in UTC.
+    return ZonedDateTime.ofInstant(instant, zoneId)
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getNoOfDays(startDate: String): String {
+    val dtf = DateTimeFormatter.ISO_DATE_TIME
+    val sdt = ZonedDateTime.parse(startDate, dtf)
+
+    val instant = getInstantTime()
+    val noOfMinutes = ChronoUnit.MINUTES.between(sdt, instant)
+    val noOfHours = ChronoUnit.HOURS.between(sdt, instant)
+    val noOfDays = ChronoUnit.DAYS.between(sdt, instant)
+    val noOfMonths = ChronoUnit.MONTHS.between(sdt, instant)
+    val noOfYears = ChronoUnit.YEARS.between(sdt, instant)
+
+
+    if(noOfYears < 1) {
+        if (noOfMonths in 1..11) {
+            return "$noOfMonths months"
+        } else {
+            if (noOfDays in 1..30) {
+                return "$noOfDays days"
+            } else {
+                if (noOfHours in 1..23) {
+                    return "$noOfHours hours"
+                } else {
+                    if (noOfMinutes in 1..59) {
+                        return "$noOfMinutes minutes"
+                    }
+                }
+            }
+        }
+    } else {
+        return "$noOfYears years"
+    }
+
+    return "0..0"
+
+
+}
